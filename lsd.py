1c885e1f7c4d92d515bfbd5591708ad48fa768d1
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-03-17 11:50:47 +0500

    Refactoring: change web output to jinja templates

--- lsd.py
+++ lsd.py
@@ -1,28 +1,15 @@
 #!python3
 
 from flask import Flask
+from flask import render_template
 from flask_cors import cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/', methods=['GET'])
 def root():
-    html = '''<html>
-<head>
-    <meta name="yandex-verification" content="f988d40377d2dd67" />
-    <style>
-    * {
-        color: white;
-        background-color: black;
-    }
-    </style>
-</head>
-<body>
-<h1>LSD Local Server Dedicated</h1>
-</body>
-</html>'''
-    return html
+    return render_template('root.html')
 
 
 @app.route('/health.php', methods=['GET'])
