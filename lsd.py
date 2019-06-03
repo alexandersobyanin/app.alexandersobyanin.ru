8b865c450b253c291ddfacc9e44ee345938605a6
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-06-04 03:10:52 +0500

    Add: Google AdSense

--- lsd.py
+++ lsd.py
@@ -18,5 +18,10 @@ def health():
     return '{"health":1}'
 
 
+@app.route('/ads.txt', methods=['GET'])
+def ads():
+    return 'google.com, pub-8901069185828760, DIRECT, f08c47fec0942fa0'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
