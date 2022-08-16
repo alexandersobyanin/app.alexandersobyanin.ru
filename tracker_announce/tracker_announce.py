f1daa768bb45c23b2001ba3dc87da3ffcdfd78fc
Alexander Sobyanin <me@alexandersobyanin.ru>
2022-08-16 18:07:20 +0500

    Feat: use TRACKER_PASS_KEY

--- tracker_announce/tracker_announce.py
+++ tracker_announce/tracker_announce.py
@@ -19,7 +19,7 @@ global_context.update(environment_variables)
 @tracker_announce.route('/<path:tracker_path>', methods=['GET'])
 def tracker_announce_process_path(tracker_path):
     pass_key = request.args.get('pk')
-    if pass_key != os.environ.get('tracker_pass_key'):
+    if pass_key != os.environ.get('TRACKER_PASS_KEY'):
         return Response(response='Unauthorized', status=401)
     params = urllib.parse.urlencode(request.args)
     url = f'http://{tracker_path}?{params}'
